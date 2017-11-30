27e2e8f9363b81ca754ad6da6014870dc6f91156
Diego <diego17almoradi@gmail.com>
2017-11-30 01:01:12 +0100

    Creados formularios de insercion y borrado

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,15 +4,32 @@
 	<title>Index</title>
 </head>
 <body>
+	<form action="{{ url('api/alumno') }}" method="POST">
+		<input type="text" name="dni" id="dni" placeholder="DNI" />
+		<input type="text" name="nombre" id="nombre" placeholder="Nombre" />
+		<input type="text" name="ape_1" id="ape_1" placeholder="Apellido 1" />
+		<input type="text" name="ape_2" id="ape_2" placeholder="Apellido 2" />
+		<input type="text" name="direccion" id="direccion" placeholder="Direccion" />
+		<input type="text" name="localidad" id="localidad" placeholder="Localidad" />
+		<input type="text" name="provincia" id="provincia" placeholder="Provincia" />
+		<input type="date" name="fecha_nac" id="fecha_nac" placeholder="Fecha de nacimiento" />
+		<input type="submit" name="submit" value="Nuevo alumno" />
+	</form>
+
 	<table>
 		@foreach ($alumnos as $alumno)
 	    	<tr>
 	    		<td>{{ $alumno->dni }}</td>
 	    		<td>{{ $alumno->nombre }}</td>
 	    		<td>{{ $alumno->ape_1 }} {{ $alumno->ape_2 }}</td>
+	    		<td>
+	    			<form method="POST" action="{{ url('api/alumno') }}/{{ $alumno->dni }}">
+	    				<input type="hidden" name="_method" value="DELETE">
+	    				<input type="submit" name="submit" value="Borrar">
+	    			</form>
+	    		</td>
 	    	</tr>
 		@endforeach
 	</table>
-	
 </body>
 </html>
